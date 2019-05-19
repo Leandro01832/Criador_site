0d3596d57921159111a64bd8bf02d3ff422ea558
Leandro <leandroleanleo@gmail.com>
2019-05-19 09:24:31 -0300

    Carousel end home index

--- Criacao_site/CriadorSites/Global.asax.cs
+++ Criacao_site/CriadorSites/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using DataContextCriacaoSite;
+﻿using CriadorSites.Models;
+using DataContextCriacaoSite;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -16,10 +19,87 @@ namespace CriadorSites
         {
 
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<BD, DataContextCriacaoSite.Migrations.Configuration>());
+            ApplicationDbContext db = new ApplicationDbContext();
+            criaroles(db);
+            criarsuperuser(db);
+            AddPermissoesSuperUser(db);
+            db.Dispose();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void AddPermissoesSuperUser(ApplicationDbContext db)
+        {
+            var usermaneger = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var user = usermaneger.FindByName("leandroleanleo@gmail.com");
+            var rolemanager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+
+            if (!usermaneger.IsInRole(user.Id, "Index"))
+            {
+                usermaneger.AddToRole(user.Id, "Index");
+            }
+
+            if (!usermaneger.IsInRole(user.Id, "Edit"))
+            {
+                usermaneger.AddToRole(user.Id, "Edit");
+            }
+
+            if (!usermaneger.IsInRole(user.Id, "Create"))
+            {
+                usermaneger.AddToRole(user.Id, "Create");
+            }
+
+            if (!usermaneger.IsInRole(user.Id, "Delete"))
+            {
+                usermaneger.AddToRole(user.Id, "Delete");
+            }
+        }
+
+        private void criarsuperuser(ApplicationDbContext db)
+        {
+            var usermaneger = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var user = usermaneger.FindByName("leandroleanleo@gmail.com");
+
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    UserName = "leandroleanleo@gmail.com",
+                    Email = "leandroleanleo@gmail.com"
+                };
+
+                usermaneger.Create(user, "Manequim 1991");
+            }
+
+        }
+
+        private void criaroles(ApplicationDbContext db)
+        {
+            var rolemanager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+
+            if (!rolemanager.RoleExists("Index"))
+            {
+                rolemanager.Create(new IdentityRole("Index"));
+            }
+
+            if (!rolemanager.RoleExists("Create"))
+            {
+                rolemanager.Create(new IdentityRole("Create"));
+            }
+
+            if (!rolemanager.RoleExists("Edit"))
+            {
+                rolemanager.Create(new IdentityRole("Edit"));
+            }
+
+            if (!rolemanager.RoleExists("Delete"))
+            {
+                rolemanager.Create(new IdentityRole("Delete"));
+            }
         }
     }
 }
